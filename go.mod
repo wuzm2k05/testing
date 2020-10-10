5b53d2b9926749268f196fbbfb9756d6fc568e1c
wuzhaoming3 <wuzhaoming3@ZBMAC-C02D51Z2M.local>
2020-10-10 17:41:51 +0800

    raft testing

---
+++ go.mod
@@ -0,0 +1,8 @@
+module traft
+
+go 1.14
+
+require (
+	github.com/hashicorp/raft v1.1.2
+	go.uber.org/zap v1.16.0
+)
---
+++ main.go
@@ -0,0 +1,120 @@
+package main
+import (
+  "time"
+  "io/ioutil"
+  "go.uber.org/zap"
+  "github.com/hashicorp/raft"
+  "fmt"
+)
+
+type RaftEnv struct {
+  dir string
+  conf *raft.Config
+  fsm *raft.MockFSM
+  store *raft.InmemStore
+  snapshot *raft.FileSnapshotStore
+  trans *raft.NetworkTransport
+  raft *raft.Raft
+}
+
+var (
+  logger *zap.Logger
+)
+
+func init() {
+  l, err := zap.NewProduction()
+  if err != nil {
+    panic("init logger error")
+  }
+  logger = l
+}
+
+
+func NoErr(err error) {
+  if err != nil {
+    logger.Panic("",zap.Error(err))
+  }
+}
+
+func waitFor(env *RaftEnv, state raft.RaftState) error {
+  limit := time.Now().Add(200 * time.Millisecond)
+  for env.raft.State() != state {
+    if time.Now().Before(limit) {
+      time.Sleep(10*time.Millisecond)
+    } else {
+      return fmt.Errorf("failed to transition to state %v",state)
+    }
+  }
+  return nil
+}
+      
+  
+func main() {
+  logger.Info("program starting...")
+  conf := raft.DefaultConfig()
+  conf.LocalID = raft.ServerID("first")
+  conf.HeartbeatTimeout = 50* time.Millisecond
+  conf.ElectionTimeout = 50* time.Millisecond
+  conf.LeaderLeaseTimeout = 50* time.Millisecond
+  conf.CommitTimeout = 5* time.Millisecond
+  conf.SnapshotThreshold = 100
+  conf.TrailingLogs = 10
+
+  //create a single node
+  env1 := makeRaft(conf, true)
+  NoErr(waitFor(env1,raft.Leader))
+  
+  
+}
+
+func makeRaft(conf *raft.Config, bootstrap bool) *RaftEnv {
+  dir,err := ioutil.TempDir("/Users/wuzhaoming3/Desktop/work/src/traft/snapshot","raft") 
+  stable := raft.NewInmemStore()
+  if err != nil {
+    panic("creating in memstore fail")
+  }
+  snap, err := raft.NewFileSnapshotStore(dir,3,nil)
+  if err != nil {
+    panic("creating snapstore fail")
+  }
+
+  env := &RaftEnv{
+    conf: conf,
+    dir: dir,
+    store: stable,
+    snapshot: snap,
+    fsm: &raft.MockFSM{},
+  }
+
+  trans, err := raft.NewTCPTransport("127.0.0.1:0",nil,2,time.Second,nil)
+  if err != nil {
+    logger.Panic("newtcptransport",zap.Error(err))
+  }
+  env.trans = trans
+
+  if bootstrap {
+    var configuration raft.Configuration
+    logger.Info("bootstaping raft cluster")
+    configuration.Servers = append(configuration.Servers, raft.Server{
+      Suffrage: raft.Voter,
+      ID: conf.LocalID,
+      Address: trans.LocalAddr(),
+    })
+  
+    err = raft.BootstrapCluster(conf, stable, stable,snap, trans, configuration)
+    if err != nil {
+     panic("bootstarpcluster fail")
+    } 
+  }
+
+  logger.Info("starting node, ", zap.String("addr:",string(trans.LocalAddr())))
+
+  //we dont need a logger for conf, since NewRaft will assign hclogger for it if there is no logger
+  raft, err := raft.NewRaft(conf, env.fsm, stable, stable, snap, trans)
+  if err != nil {
+    panic("staring a new raft fail")
+  }
+  env.raft = raft
+
+  return env
+} 
