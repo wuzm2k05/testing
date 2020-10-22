53df113222cfee11972c4eea77ee45fc277ae5d8
wuzm <wuzm2k05@hotmail.com>
2020-10-22 21:12:22 +0800

    kongkong

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
   "go.uber.org/zap"
   "github.com/hashicorp/raft"
   "fmt"
+  "strconv"
 )
 
 type RaftEnv struct {
@@ -30,6 +31,14 @@ func init() {
   logger = l
 }
 
+func WaitFuture(f raft.Future) error {
+  timer := time.AfterFunc(200*time.Millisecond,func(){
+    panic(fmt.Errorf("timeout waiting for future %v",f))
+  })
+  defer timer.Stop()
+  return f.Error()
+}
+
 
 func NoErr(err error) {
   if err != nil {
@@ -48,8 +57,49 @@ func waitFor(env *RaftEnv, state raft.RaftState) error {
   }
   return nil
 }
+
+func WaitForAny(state raft.RaftState, envs []*RaftEnv) (*RaftEnv, error){
+  limit := time.Now().Add(200 * time.Millisecond)
+CHECK:
+  for _, env := range envs {
+    if env.raft.State() == state {
+      return env, nil
+    }
+  }
+
+  if time.Now().Before(limit) {
+    goto WAIT
+  }
+  return nil, fmt.Errorf("failed to find node in %v state",state)
+WAIT:
+  time.Sleep(10*time.Millisecond)
+  goto CHECK
+}
       
+
+func leaderAction(conf raft.Config, serverID string, port string){
+  //create a single leader node
+  conf.LocalID=raft.ServerID(serverID)
+  env1 := makeRaft(conf, true,port)
+  NoErr(waitFor(env1,raft.Leader))
+
+  //add one voter
   
+}
+
+func voterAction(conf raft.Config, serverID string, port string){
+  conf.LocalID=raft.ServerID(serverID)
+  env := makeRaft(conf,false,port)
+  addr := env.trans.LocalAddr()
+  NoErr(WaitFuture(env1.raft.AddVoter(conf.LocalID,addr,0,0)))
+}
+
+/*
+parameter:
+usage:  traft [l|c] [ServerID] [port]
+Des: for leader:
+    Add Voter: [serverID] [port] 
+*/  
 func main() {
   logger.Info("program starting...")
   conf := raft.DefaultConfig()
@@ -61,23 +111,42 @@ func main() {
   conf.SnapshotThreshold = 100
   conf.TrailingLogs = 10
 
+  if len(os.Args) != 4 {
+    logger.Panic("args Error")
+  }
+
+  if os.Args[1] == "l" {
+    leaderAction()
+  }else{
+    voterAction()
+  }
   //create a single node
-  env1 := makeRaft(conf, true)
+  env1 := makeRaft(conf, true,"9000")
   NoErr(waitFor(env1,raft.Leader))
 
   //join a few nodes
   var envs []*RaftEnv
   for i := 0; i<2; i++ {
-    conf.LocalID = ServerID(fmt.Sprintf("next-batch-%d",i))
-    env := makeRaft(conf,false)
+    conf.LocalID = raft.ServerID(fmt.Sprintf("next-batch-%d",i))
+    env := makeRaft(conf,false,strconv.Itoa(9000+i+1))
     addr := env.trans.LocalAddr()
-    NoErr(
+    NoErr(WaitFuture(env1.raft.AddVoter(conf.LocalID,addr,0,0)))
+    envs = append(envs,env)
   }
-    
+
+  leader, err := WaitForAny(raft.Leader, append([]*RaftEnv{env1},envs...))
+  NoErr(err)
+
+  leader = leader
+ 
+  
+  for{
+    time.Sleep(3000*time.Millisecond)
+  }
   
 }
 
-func makeRaft(conf *raft.Config, bootstrap bool) *RaftEnv {
+func makeRaft(conf *raft.Config, bootstrap bool, port string) *RaftEnv {
   stable := raft.NewInmemStore()
   if _, err := os.Stat("snapshot"); os.IsNotExist(err) {
     err := os.Mkdir("snapshot",0755)
@@ -101,7 +170,7 @@ func makeRaft(conf *raft.Config, bootstrap bool) *RaftEnv {
     fsm: &raft.MockFSM{},
   }
 
-  trans, err := raft.NewTCPTransport("127.0.0.1:0",nil,2,time.Second,nil)
+  trans, err := raft.NewTCPTransport("127.0.0.1:"+port,nil,2,time.Second,nil)
   if err != nil {
     logger.Panic("newtcptransport",zap.Error(err))
   }
